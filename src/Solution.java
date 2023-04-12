f3cff62133383ea457ed2a69f1a6ef51d6d7dc65
KANG-MoonSoo <munsu2483@gmail.com>
2023-04-12 13:05:19 +0900

    first commit

--- src/Solution.java
+++ src/Solution.java
@@ -2,12 +2,12 @@ import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
-        String my_string = "abcdef";
-        String letter = "b";
-        String answer = "";
-        for (int i = 0; i < my_string.length(); i++) {
-            if (my_string.charAt(i) != letter.charAt(0)){
-                answer += my_string.charAt(i);
+       int n = 30;
+       int answer = 0;
+        for (int i = 0; i <= n; i++) {
+            int k = n - i;
+            if (k % 2 == 0){
+                answer += k;
             }
         }
         System.out.println(answer);
